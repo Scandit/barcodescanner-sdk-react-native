854bcb519c2c0d93614997e0d8bb72bea25cfb3a
Tibor Molnár <tibor@scandit.com>
2018-10-08 16:42:17 +0200

    Add Lensposition setting to Settings tab
    Signed-off-by: Tibor Molnár <tibor@scandit.com>

--- samples/ExtendedSample/app/tabs/ScanTab.js
+++ samples/ExtendedSample/app/tabs/ScanTab.js
@@ -116,6 +116,8 @@ export default class ScanScreen extends Component {
     this.scanSpecs.scanSettings.setSymbologyEnabled(Barcode.Symbology.DOTCODE, false);
     this.scanSpecs.scanSettings.setSymbologyEnabled(Barcode.Symbology.MICROQR, false);
     this.scanSpecs.scanSettings.setSymbologyEnabled(Barcode.Symbology.CODE32, false);
+    this.scanSpecs.extension.lensPositionEnabled = false;
+    this.scanSpecs.extension.lensPosition = 50;
     this.scanSpecs.scanSettings.symbologies[Barcode.Symbology.QR].colorInvertedEnabled = false;
     this.scanSpecs.scanSettings.symbologies[Barcode.Symbology.DATA_MATRIX].colorInvertedEnabled = false;
     this.scanSpecs.scanSettings.activeScanningAreaCenterY = 0.5;
